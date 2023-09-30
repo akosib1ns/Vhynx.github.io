a23b894a2ad5a07bd94f76c11757f1121d2e3d00
Vhynx04 <delo04santos16@gmail.com>
2023-09-30 16:19:01 +0800

    updated

--- cars.js
+++ cars.js
@@ -1,72 +1,74 @@
 export const cars = {
     Nissan_Leaf_S: [
-        'Nissan Leaf S',
-        'Overview Manufacturer	Nissan Production	December 2010–present Model years	2011–present (Europe & North America)Body and chassis Class	Compact/Small family car (C)[1] Body style	5-door hatchback Layout	Front-motor, front-wheel-drive Chronology Predecessor	Nissan Altra'
+        "Nissan Leaf S",
+        "Overview Manufacturer	Nissan Production	December 2010–present Model years	2011–present (Europe & North America)Body and chassis Class	Compact/Small family car (C)[1] Body style	5-door hatchback Layout	Front-motor, front-wheel-drive Chronology Predecessor	Nissan Altra"
     ],
-     Tesla_Model_S_Plaid: [
+    Tesla_Model_S_Plaid: [
         "Tesla Model S Plaid",
-        "Overview Manufacturer	Tesla, Inc. Production	2012–present Assembly  United States: Fremont, California (Fremont Factory) Netherlands: Tilburg (Tilburg final assembly, 2013–2021) Designer	Franz von Holzhausen Body and chassis Class	Full-size luxury car (E) Body style	5-door sedan liftback Layout	 Rear-motor, rear-wheel drive Dual-motor, all-wheel drive (D models) Related	Tesla Model X Powertrain Engine	3-phase AC induction motor Electric motor	Front and rear motor combined output up to 615 kW (825 bhp), 1,300 N⋅m (960 lb⋅ft), 3-phase AC induction motor Transmission	1-speed fixed gear ratio (9.734:1 or 9.325:1); direct-drive[1][2] Battery	100 kWh lithium ion Discontinued: 60, 70, 75, 85, 90 kWh Electric range	75 kWh (270 MJ) 249–259 mi (401–417 km) (EPA) 85 kWh (310 MJ) 253–272 mi (407–438 km) (EPA) 310 mi (500 km) (NEDC) 90 kWh (320 MJ) 270–294 mi (435–473 km) (EPA) 100 kWh (360 MJ) 348–402 mi (560–647 km) (EPA) Plug-in charging	 Onboard charger 3ϕ 400 V 16 A (Type 2);[3] 1ϕ 85–300 V 48 A (new front fascia variant) Optional charger: 20 kW from 1ϕ 277 V 72 A Dual charger: 21.1 kW from 1ϕ 264 V 80 A and 22 kW from 3ϕ 400 V 32 A; 19.2 kW from 2ϕ 240 V 80 A[4] (old front fascia variant); 16.5 kW from 3ϕ 400 V 24 A (new front fascia variant)[citation needed] Offboard charger Supercharger at 150–250 kW DC Dimensions Wheelbase	116.5 in (2,960 mm) Length	195.9 in (4,980 mm) Width	 77.3 in (1,964 mm) (ex. mirrors) 86.2 in (2,189 mm) (inc. mirrors) Height	56.5 in (1,440 mm)Curb weight	4,323–4,960 lb (1,961–2,250 kg)[5][6][7]"
-     ],
+        "Tesla Model S Plaid: A high-performance electric sedan by Tesla, known for its incredible acceleration (0-60 mph in under 2 seconds) and top speed exceeding 200 mph. It offers a range of approximately 390 miles on a full charge, features tri-motor AWD, minimalist interior with a large touchscreen, supports Tesla's Autopilot and Full Self-Driving (FSD), and is compatible with Tesla Superchargers. Price varies based on configurations. For the latest details, visit Tesla's official website."
+    ],
     Audi_E_Tron: [
         "Audi E-Tron",
-        "Overview Manufacturer Audi Also called	Audi e-tron (2018–2023) Production	2018–present Assembly	Belgium: Forest (Audi Brussels) China: Changchun (FAW-VW) Designer	Siming Yan Body and chassis Class	Mid-size luxury crossover SUV Body style	5-door SUV 5-door coupé SUV (Sportback) Layout	Dual or Triple-motors, four-wheel drive Platform	Volkswagen Group MLB Evo[1] Powertrain Electric motor	 2x Asynchronous motors (ASMs)[2] 3x Asynchronous motors (ASMs) (SQ8 e-tron) Power output	Pre-update 50 quattro: 230 kW (308 hp) 55 quattro: 300 to 370 kW (402 to 496 hp) S quattro: 370 kW (496 hp) Post-Update (Q8 e-tron) Q8 50 e-tron: 183 kW (245 hp) Q8 55 e-tron: 220 kW (295 hp) SQ8 e-tron : 370 kW (496 hp) Transmission	Single speed with fixed ratio Battery	55 quattro, post-update: 95 kWh lithium ion (86.5 kWh usable) 55 quattro: 95 kWh lithium ion (83.6 kWh usable) 50 quattro: 71 kWh lithium ion (64.7 kWh usable) Q8 e-tron Series: 89 – 114 kWh lithium ion Plug-in charging	EU: 11 kW AC 22 kW AC (optional) US: 9.6 kW AC All markets: 150 kW DC[note 1] Dimensions Wheelbase	2,928 mm (115.3 in) Length	4,901 mm (193.0 in) Width	1,935 mm (76.2 in) Height	1,616 mm (63.6 in) Kerb weight	~2,560 kg (5,643.8 lb)"
+        "Audi e-tron: An electric SUV lineup by Audi, featuring models like the e-tron SUV and e-tron Sportback. Known for its premium interior, quattro all-wheel drive, and a range of approximately 200-250 miles. Offers advanced technology and is positioned as a luxury electric vehicle. For the latest details, visit Audi's official website."
     ],
     Nissan_Versa_1_6_SV: [
         "Nissan Versa 1.6 SV",
-        "Hatdog"
+        "Nissan Versa 1.6 SV: A compact sedan known for its affordability and fuel efficiency. Features typically include a 1.6-liter engine, automatic CVT transmission, user-friendly interior, and modern tech options like touchscreen infotainment. Positioned as a budget-friendly choice in the Versa lineup. For the latest details, visit Nissan's official website."
 
     ],
     Toyota_Vios_E: [
         "Toyota Vios E",
-        "Wala pa"
+        "Toyota Vios E: A compact sedan known for its reliability and affordability. Features typically include a fuel-efficient engine, comfortable interior, and basic tech options. Positioned as a practical and budget-friendly choice in the Toyota Vios lineup. For the latest details, visit Toyota's official website."
 
     ],
     Honda_Civic_ES_L: [
         "Honda Civic ES-L",
-        "kukuha pa sa google"
+        "The Honda Civic is a popular compact car available in various body styles (sedan, coupe, hatchback), with multiple engine and transmission options. It offers features like advanced safety tech, infotainment systems, and good fuel efficiency. Honda emphasizes reliability, making the Civic a trusted choice for many buyers worldwide. Specific features and availability may vary by market and model year."
 
     ],
     Honda_Insight: [
         "Honda Insight",
-        "ano daw"
+        "The Honda Insight is a compact hybrid sedan known for its exceptional fuel efficiency. It combines a gasoline engine with an electric motor and typically offers modern features, including advanced safety technologies and infotainment systems. The Insight is designed with eco-friendliness in mind and is available in various markets worldwide. Specific features and availability may vary by region and model year."
 
     ],
-    Toyota_Prius_LE:[
+    Toyota_Prius_LE: [
         "toyota Prius LE",
-        "maya"
+        "The Toyota Prius LE is a trim level of the Toyota Prius, a renowned hybrid car known for its exceptional fuel efficiency and eco-friendly attributes. It features a hybrid powertrain, typically with a gasoline engine and an electric motor, and offers modern amenities, including advanced safety technologies and infotainment systems. The Prius LE is often available as a compact hatchback and emphasizes eco-friendliness. Specific features and availability may vary by region and model year."
 
     ],
     Mitsubishi_Outlander_ES: [
         "Mitsubishi Outlander ES",
-        "hakdog moto"
+        "The Mitsubishi Outlander ES is a trim level of the Mitsubishi Outlander, a midsize SUV. It typically offers versatile seating for up to seven passengers, with various engine options, including gasoline and hybrid powertrains. Standard features may include a touchscreen infotainment system and basic safety features. The Outlander ES is known for its spacious interior and flexible cargo space. Specific features and availability can vary by region and model year, so it's advisable to consult Mitsubishi's official website or a dealership for the most current information."
 
     ],
     Toyota_Fortuner: [
         "Toyota Fortuner",
-        "bukas nalang"
+        "The Toyota Fortuner is a rugged midsize SUV known for its off-road capabilities. It comes in various configurations and engine options, including gasoline and diesel. The vehicle can typically seat seven passengers and offers features such as advanced safety tech and off-road enhancements. Availability and specifications may vary by region and model year. Toyota is known for its reputation for reliability and durability, making the Fortuner a popular choice in multiple markets. For precise details, it's advisable to consult Toyota's official website or a local dealership."
 
     ],
     Mitsubishi_Montero: [
         "Mitsubishi Montero Sport",
-        "mama mo"
+        "The Mitsubishi Montero Sport is a rugged midsize SUV designed for both on-road and off-road use. It's available with various engine options, including gasoline and diesel, and offers configurations with up to seven seats. Features can include advanced infotainment systems and off-road enhancements, with availability varying by region and model year. Mitsubishi vehicles, including the Montero Sport, are known for their reliability. To get precise details, it's best to consult Mitsubishi's official website or a local dealership, as specifications can differ significantly based on location and model year."
 
     ],
     Nissan_Terra: [
         "Nissan Terra",
-        "panot ka"
+        "The Nissan Terra is a midsize SUV designed primarily for Asian and Middle Eastern markets. It offers various engine options, often with diesel engines, and comes in both 2WD and 4WD configurations. The vehicle typically seats up to seven passengers, making it suitable for families, and it may feature modern amenities like touchscreen infotainment systems. The Terra is designed for off-road capabilities, and some models include advanced off-road systems. Towing capacity can be respectable, depending on the configuration. Availability and features vary by region and model year. Nissan is generally known for producing reliable vehicles. For precise details, it's advisable to consult Nissan's official website or a local dealership."
 
     ],
     Nissan_Navara: [
         "Nissan Navara",
-        "oca"
+        "The Nissan Navara is a versatile midsize pickup truck known for its off-road capabilities. It comes with a range of engine options, including gasoline and diesel, and is available in both 2WD and 4WD configurations. The Navara offers various cab styles, making it adaptable for different needs, and often includes modern features like touchscreen infotainment systems. It's designed for both work and recreational use, with impressive payload and towing capacities. Availability and features can vary by region and model year. Nissan is generally known for producing reliable vehicles. For precise details, it's advisable to consult Nissan's official website or a local dealership."
 
     ],
     Mitsubishi_Strada: [
         "Mitsubishi Strada",
-        "panochi"
+        "The Mitsubishi Strada is a rugged and versatile midsize pickup truck designed for on-road and off-road use. It offers a range of engine options, including gasoline and diesel, and comes in both 2WD and 4WD configurations. The Strada is available in various cab styles, making it adaptable for different purposes, and often features modern amenities like touchscreen infotainment systems. It's capable of handling payloads and towing, suitable for both work and recreation. Availability and features can vary by region and model year. Mitsubishi is known for producing reliable vehicles. For precise details, it's advisable to consult Mitsubishi's official website or a local dealership."
 
     ],
-    Toyota_Hilux: ["akoak","akokao"
+    Toyota_Hilux: [
+        "Toyota Hilux",
+        "The Toyota Hilux is a durable and versatile midsize pickup truck designed for various driving conditions, including off-road. It offers a range of engine options, including gasoline and diesel, and comes in both 2WD and 4WD configurations. The Hilux is available in different cab styles and often includes modern features like touchscreen infotainment systems. It's capable of handling payloads and towing, making it suitable for work and leisure. Availability and features can vary by region and model year. Toyota's reputation for reliability and durability extends to the Hilux, making it a popular choice worldwide. For precise details, it's advisable to consult Toyota's official website or a local dealership."
     ]
 
 }
